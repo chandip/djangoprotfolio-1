6396303841c899c4a36634b57e7cbb016241acc8
chandip <maskeyc@gmail.com>
2019-04-13 16:17:52 -0400

    nginx conf changed

--- madzones/madzones/settings/prod.py
+++ madzones/madzones/settings/prod.py
@@ -8,27 +8,14 @@ DEBUG = False
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_files")
 
 STATIC_URL = "/assets/"
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR, "assets"))
-# BASE_DIR is where manage.py lives. above code goes one step up the manage.py
+# BASE_DIR is where manage.py lives. os.path.dirname code goes one step up the manage.py
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
-
-SECRET_KEY = "asdfasefsdfef3323654DDDcdsaESDG2"
+SECRET_KEY = "change@it@to@your@own"
 
 ALLOWED_HOSTS = [
     "127.0.0.1:8000"
     "127.0.0.1"
 ]
 
-
-# from madzones.settings.base import *
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-#
-# # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "asdfasefsdfef3323654DDDcdsaESDG2"
-#
-# ALLOWED_HOSTS = ["*"]
-#
