c91e6db0619f860fceead2d27bbc3d18f72011c0
chandip <maskeyc@gmail.com>
2019-04-24 09:00:24 -0400

    blog front page maintain

--- madzones/madzones/settings/base.py
+++ madzones/madzones/settings/base.py
@@ -82,9 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'madzones',
-        'USER': 'madzones',
+        'USER': 'postgres',
         'PASSWORD': 'madzones',
-        'HOST': 'postgres',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
